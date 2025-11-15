b4087db05b6d637f850dbc4a54baac0e460a33e6
mafer469 <mafer.cs2020@gmail.com>
2025-11-15 10:39:06 -0500

    Update main.jsx to handle frontend login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
+import Login from './login.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Root() {
+  const [user, setUser] = useState(null); //no user yet -> show login
+
+  // if no user, render login
+  if (!user) {
+    return <Login onLogin={setUser} />;
+  }
+
+  // after login, render App
+  return <App user={user} />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
